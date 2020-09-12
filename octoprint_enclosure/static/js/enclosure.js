7376186469e80bff8d90b581f9af774c9938e18e
Raymond de Hooge <Raymond.dehooge@gmail.com>
2020-09-12 20:19:09 +0200

    octoprint keeps loading ui :/

--- octoprint_enclosure/static/js/enclosure.js
+++ octoprint_enclosure/static/js/enclosure.js
@@ -13,21 +13,6 @@ $(function () {
 
     self.settingsOpen = false;
 
-    var ClickCounterViewModel = function() {
-      this.numberOfClicks = ko.observable(0);
-   
-      this.registerClick = function() {
-          this.numberOfClicks(this.numberOfClicks() + 1);
-      };
-   
-      this.resetClicks = function() {
-          this.numberOfClicks(0);
-      };
-   
-      this.hasClickedTooManyTimes = ko.pureComputed(function() {
-          return this.numberOfClicks() >= 3;
-      }, this);
-    };ko.applyBindings(new ClickCounterViewModel());
 
     self.settings_outputs_regular = ko.pureComputed(function () {
       return ko.utils.arrayFilter(self.settingsViewModel.settings.plugins.enclosure.rpi_outputs(), function (item) {
