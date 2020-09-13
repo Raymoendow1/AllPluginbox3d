b36a2630e67c9caca9ef442d172e5e8fc9941f60
Raymond de Hooge <Raymond.dehooge@gmail.com>
2020-09-13 16:24:14 +0200

    changed checkbox with text input

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -483,7 +483,7 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
 
         command = rpi_output['shell_script'] #+ ' ' + value
         self._logger.info("Send script command: " + command)
-        self.logger.info(str(value))
+        self._logger.info(str(value))
 
         # self.shell_command(command)
         return jsonify(success=True)
