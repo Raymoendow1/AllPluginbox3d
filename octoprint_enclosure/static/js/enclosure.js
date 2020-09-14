5cd02de4903ff7466bcac054032bd04f32e43d3b
Raymond de Hooge <Raymond.dehooge@gmail.com>
2020-09-14 04:57:49 +0200

    use_slider implementation

--- octoprint_enclosure/static/js/enclosure.js
+++ octoprint_enclosure/static/js/enclosure.js
@@ -43,6 +43,7 @@ $(function () {
       });
     });
 
+    self.use_slider = ko.observable();
     self.use_sudo = ko.observable();
     self.gcode_control = ko.observable();
     self.neopixel_dma = ko.observable();
@@ -282,6 +283,7 @@ $(function () {
     self.bindFromSettings = function(){
       self.rpi_outputs(self.settingsViewModel.settings.plugins.enclosure.rpi_outputs());
       self.rpi_inputs(self.settingsViewModel.settings.plugins.enclosure.rpi_inputs());
+      self.use_slider(self.settingsViewModel.settings.plugins.enclosure.use_slider());
       self.use_sudo(self.settingsViewModel.settings.plugins.enclosure.use_sudo());
       self.gcode_control(self.settingsViewModel.settings.plugins.enclosure.gcode_control());
       self.neopixel_dma(self.settingsViewModel.settings.plugins.enclosure.neopixel_dma());
